b9ef3fd8f77d529c47f0f46df8c8e9276619a43a
Karanveer Singh <157493365+singhhkv@users.noreply.github.com>
2026-02-10 14:39:06 +0530

    Update base URL in Vite configuration
    Change base URL for deployment configuration.

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Karanveer-Portfolio2', // 👈 this fixes the blank page on deployment
+  base: '/',
 })
